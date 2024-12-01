e33e739ee9ab5def80783add31ed60193755a357
ronik-v <andronnikovvv@gmail.com>
2024-12-01 21:57:21 +0300

    query fix

--- src/main/java/com/geotraveljournal/app/dao/auth/UserTokenDao.java
+++ src/main/java/com/geotraveljournal/app/dao/auth/UserTokenDao.java
@@ -1,13 +1,13 @@
-    package com.geotraveljournal.app.dao.auth;
+package com.geotraveljournal.app.dao.auth;
 
-    import com.geotraveljournal.app.model.auth.UserToken;
-    import org.springframework.data.jpa.repository.JpaRepository;
-    import org.springframework.data.jpa.repository.Query;
-    import org.springframework.data.repository.query.Param;
-    import org.springframework.stereotype.Repository;
+import com.geotraveljournal.app.model.auth.UserToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-    @Repository
-    public interface UserTokenDao extends JpaRepository<UserToken, Long> {
-        @Query("SELECT ut FROM UserToken ut WHERE ut.userId = :currentUserId")
-        UserToken getCurrentUserToken(@Param("currentUserId") Long currentUserId);
-    }
+@Repository
+public interface UserTokenDao extends JpaRepository<UserToken, Long> {
+    @Query("SELECT ut FROM UserToken ut WHERE ut.userId = :currentUserId")
+    UserToken getCurrentUserToken(@Param("currentUserId") Long currentUserId);
+}
