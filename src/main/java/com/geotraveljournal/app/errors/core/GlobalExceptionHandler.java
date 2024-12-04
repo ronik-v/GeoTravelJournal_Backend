4e5d0d9a833dcfdc03e707f32be4ae635041a8ea
ronik-v <andronnikovvv@gmail.com>
2024-12-04 20:20:32 +0300

    update: add exception handler

---
+++ src/main/java/com/geotraveljournal/app/errors/core/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.geotraveljournal.app.errors.core;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<Object> handleCustomException(CustomException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getStatus(),
+                ex.getMessage()
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    public static class ErrorResponse {
+        private final boolean status;
+        private final String message;
+
+        public ErrorResponse(boolean status, String message) {
+            this.status = status;
+            this.message = message;
+        }
+
+        public boolean isStatus() {
+            return status;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
